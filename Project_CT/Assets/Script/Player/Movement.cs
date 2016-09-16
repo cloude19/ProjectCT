2b412177369cefdacdacf2137a5331d7d7f12095
cloude19 <troycloude@gmail.com>
2016-09-16 00:20:29 -0400

    Player movement/Collision
    using the character controller the player now has control of the box
    using the x and y axis. Also as a result of using the character
    controller collison is already done. Just need artifical boundaries.

---
+++ Project_CT/Assets/Script/Player/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float horizontalMOV = 0;
+    public float VerticalMOV = 0;
+    public float speed = 0;
+
+    private CharacterController _charController;
+
+
+
+    // Use this for initialization
+    void Start () {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //currently has acceleration 
+        //Note: try to use character controller
+        horizontalMOV = Input.GetAxisRaw("Horizontal");
+        VerticalMOV = Input.GetAxisRaw("Vertical");
+        
+
+
+        Vector3 movement = new Vector3(horizontalMOV* speed, VerticalMOV* speed, 0.0f);
+        
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement *= Time.deltaTime;
+        _charController.Move(movement);
+
+
+
+        //   transform.position += Vector3.ClampMagnitude(movement, speed) * Time.deltaTime;
+
+    }
+}
