43828f0dbadfa2e121bc6689d8eaa40fd02e82b6
cloude19 <troycloude@gmail.com>
2016-09-19 16:39:15 -0400

    Boundaries
    using world corrdinates the player is now bound to a 11x5 rectangle.

---
+++ Project_CT/Assets/Script/Camera/Boundary.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+
+    Camera main_camera;
+    public Transform player;
+    public Vector3 screenPos;
+
+    private double fps = 0;
+
+    // Use this for initialization
+    void Start () {
+        main_camera = GetComponent<Camera>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //get player postion compared to camera
+        screenPos = main_camera.WorldToScreenPoint(player.position);
+        //   Debug.Log("player X postion is" + screenPos.x + "Player Y postion is" + screenPos.y);
+        fps = 1.0 / Time.deltaTime;
+        Debug.Log("Current fps: " + fps);
+
+    }
+}
--- Project_CT/Assets/Script/Player/Movement.cs
+++ Project_CT/Assets/Script/Player/Movement.cs
@@ -6,31 +6,58 @@ public class Movement : MonoBehaviour {
     public float horizontalMOV = 0;
     public float VerticalMOV = 0;
     public float speed = 0;
+    public GameObject Player;
 
     private CharacterController _charController;
+    private Boundary player_POS;
 
 
-
-    // Use this for initialization
-    void Start () {
+// Use this for initialization
+void Start () {
         _charController = GetComponent<CharacterController>();
+
+        //needed to direct boundary to the main camera version.
+        player_POS = GameObject.Find("Main Camera").GetComponent<Boundary>();
     }
 
     // Update is called once per frame
     void Update () {
+
+
 
         //currently has acceleration 
         //Note: try to use character controller
         horizontalMOV = Input.GetAxisRaw("Horizontal");
         VerticalMOV = Input.GetAxisRaw("Vertical");
         
-
+        
 
         Vector3 movement = new Vector3(horizontalMOV* speed, VerticalMOV* speed, 0.0f);
         
         movement = Vector3.ClampMagnitude(movement, speed);
-        movement *= Time.deltaTime;
+        movement *= Time.smoothDeltaTime;
         _charController.Move(movement);
+
+
+        //bounds the player character to a 11x5 retangle based off world coordinates
+        if (Player.transform.position.x <= -11f)
+        {
+            Player.transform.position = new Vector3(-11,Player.transform.position.y,Player.transform.position.z);
+        }
+        else if (Player.transform.position.x > 11f)
+        {
+            Player.transform.position = new Vector3(11, Player.transform.position.y, Player.transform.position.z);
+        }
+        if (Player.transform.position.y >= 5)
+        {
+            Player.transform.position = new Vector3(Player.transform.position.x, 5, Player.transform.position.z);
+        }
+        else if (Player.transform.position.y <= -5)
+        {
+            Player.transform.position = new Vector3(Player.transform.position.x, -5, Player.transform.position.z);
+        }
+
+
 
 
 
