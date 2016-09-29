f3e199a98641f425f7d010e7e0c25fd45b6a0eeb
cloude19 <troycloude@gmail.com>
2016-09-29 13:17:13 -0400

    bullet destrution/Player prefab
    added bullet destroy boundary, and also set the Player object as a
    prefab to use between scenes.
    Note: to change bullet direction the actual rotation of the bullet
    prefab needs to change. it's local axis with change with the object

--- Project_CT/Assets/Script/Player/Bullet.cs
+++ Project_CT/Assets/Script/Player/Bullet.cs
@@ -23,6 +23,26 @@ public class Bullet : MonoBehaviour {
         */
         transform.Translate(0, 0, speed * Time.deltaTime);
 
+        /*Destroy bullet object if it's out side of these boundaries [can be universal]
+         * worst case is 12 bullet object at once at current speed
+        */
+        if (gameObject.transform.position.x <= -15f)
+        {
+            Destroy(gameObject);
+        }
+        else if (gameObject.transform.position.x > 15f)
+        {
+            Destroy(gameObject);
+        }
+        if (gameObject.transform.position.y >= 10)
+        {
+            Destroy(gameObject);
+        }
+        else if (gameObject.transform.position.y <= -10)
+        {
+            Destroy(gameObject);
+        }
+
 
     }
 }
